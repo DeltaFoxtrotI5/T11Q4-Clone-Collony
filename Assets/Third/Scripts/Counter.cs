ec3260e0268f1f311e25df460a0985139e1fda4d
Dilan Cha <1711753@mukilteo.wednet.edu>
2022-03-24 13:38:27 -0700

    button stuff

---
+++ Assets/Third/Scripts/Counter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    private Text score;
+    public int Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+        score = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+   private void Update()
+    {
+        score.text = Score.ToString();
+    }
+
+
+   public void AddScore()
+   {
+        Score += 1;
+   }
+}
