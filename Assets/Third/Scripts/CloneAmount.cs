53a3e9dba7a40271b4872a63fcf1bc26a05e9f1c
Dilan Cha <1711753@mukilteo.wednet.edu>
2022-04-15 11:41:30 -0700

    Clone Stuff

---
+++ Assets/Third/Scripts/CloneAmount.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneAmount : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length > 20)
+        {
+            // Do something
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
