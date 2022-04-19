a45c50c7edc52e7a40c93ae7f7b783afb62a980a
MUKILTEO\1710220 <griffindbarlow@gmail.com>
2022-04-19 12:05:07 -0700

    Credits and Tutorial stage 1

---
+++ Assets/Second/Credits/ToC.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToC : MonoBehaviour
+{
+
+    public void OnButtonPress()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
