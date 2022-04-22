48f41c6fd2a377be1ab19a5da17980157e32fd9d
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-04-22 11:59:27 -0700

    Made Clone Counter.

--- Assets/BenStuff/Assets/Scripts/Score.cs
+++ Assets/BenStuff/Assets/Scripts/Score.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
     public int boidNumber;
+    public Text uiText;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +26,7 @@ public class Score : MonoBehaviour
         {
             Debug.Log("Lose");
         }
+
+        uiText.text = boidNumber.ToString();
     }
 }
