7df1bf974c7cd963d994d83c0296cf5b1405630e
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-04-22 12:26:17 -0700

    coordinate.

---
+++ Assets/BenStuff/Assets/Scripts/coordinates.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class coordinates : MonoBehaviour
+{
+    public int X;
+    public int Y;
+
+    public Text uitextX;
+    public Text uitectY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uitextX.text = this.gameObject.transform.position.x.ToString();
+        uitectY.text = this.gameObject.transform.position.y.ToString();
+    }
+}
