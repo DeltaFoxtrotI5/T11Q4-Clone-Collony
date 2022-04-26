46c170ff5229923eb17fb8377438fecd52dbcf8e
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-04-26 13:14:44 -0700

    Made pause menu.

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        pauseMenuUI.SetActive(false);
+        
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting game...");
+        Application.Quit();
+    }
+}
---
+++ Assets/StopMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PauseMenu.GameIsPaused)
+        {
+            this.gameObject.GetComponent<AudioSource>().pitch *= 0;
+        }
+        else
+        {
+            this.gameObject.GetComponent<AudioSource>().pitch *= 1;
+        }
+    }
+}
